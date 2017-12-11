3c6617bfad5189bd51e26793fa5e2ab14a4820f4
taidalab <zdajun1988@163.com>
2017-12-11 21:17:56 +0800

    gcc *.c 编译出来的a.out文件

---
+++ project.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>  
+#include <unistd.h>  
+#include <fcntl.h>  
+#include "juv.h"  
+#include "CameralOpt.h"  
+#include "FrameBufferOpt.h"  
+  
+#define    WIDTH   640  
+#define    HIGHT   480  
+  
+int main(void)  
+{  
+    char yuyv[WIDTH*HIGHT*2];  
+    char bmp[WIDTH*HIGHT*3];  
+  
+//  set_bmp_header((struct bmp_header_t *)bmp, WIDTH, HIGHT);  
+    //初始化摄像头  
+    Init_Cameral(WIDTH , HIGHT );  
+    //初始化framebuffer  
+    Init_FrameBuffer(WIDTH , HIGHT );   
+  
+    //开启摄像头  
+    Start_Cameral();  
+    //采集一张图片  
+    int count = 0 ;   
+    while(1)  
+    {  
+        Get_Picture(yuyv);  
+        yuyv2rgb24(yuyv, bmp, WIDTH, HIGHT);  
+        Write_FrameBuffer(bmp);  
+//      printf("count:%d \n" , count++);  
+    }  
+    //关闭摄像头  
+    Stop_Cameral();  
+    //关闭Framebuffer  
+    Exit_Framebuffer();  
+    //退出  
+    Exit_Cameral();  
+      
+    return 0;  
+}  
